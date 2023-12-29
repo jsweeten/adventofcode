5768704e4e9d3f5f265a58c1b852be17b32f3388
Justin Sweeten <jtsweeten@gmail.com>
2023-12-29 13:15:22 -0500

    Day 1 part 2 complete

--- csharp/2022/AdventOfCode2022/Day01_2/Program.cs
+++ csharp/2022/AdventOfCode2022/Day01_2/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main()
+    {
+        List<int> elves = new();
+        int current = 0;
+        string line;
+
+        try
+        {
+            var sr = new StreamReader("input.txt");
+            line = sr.ReadLine();
+
+            while (line != null)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    elves.Add(current);
+                    current = 0;
+                }
+                else
+                {
+                    var calories = int.Parse(line);
+                    current += calories;
+                }
+                line = sr.ReadLine();
+            }
+
+            elves.Add(current);
+            sr.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            var total = elves.OrderByDescending(x => x).Take(3).Sum();
+            
+            Console.WriteLine("Calories from Top Three Elves: " + total);
+            Console.ReadLine();
+        }
+    }
+}
